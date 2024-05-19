e24bf7538719d7db8aaa016c3297a611a6e4a5b5
lokeshkumar.thyagarajan <LokeshKumar.Thyagarajan@hidglobal.com>
2024-05-19 13:24:17 +0530

    Day 6 code added

---
+++ src/main/java/learning/IosFirstCodeWithBundleIdCapability.java
@@ -0,0 +1,24 @@
+package learning;
+
+import io.appium.java_client.AppiumDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.testng.annotations.Test;
+
+import java.io.File;
+import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URISyntaxException;
+
+public class IosFirstCodeWithAppCapability {
+    @Test
+    public void runCode() throws URISyntaxException, MalformedURLException {
+        DesiredCapabilities dc = new DesiredCapabilities();
+        dc.setCapability("platformName", "iOS");
+        dc.setCapability("automationName", "XCUITest");
+        dc.setCapability("udid","1A69E837-798C-40B3-9229-BA47180D13F2");
+        dc.setCapability("app", System.getProperty("user.dir") + File.separator + "apks" + File.separator + "UiCatalog.zip");
+        AppiumDriver driver = new AppiumDriver(new URI("http://127.0.0.1:4723/").toURL(), dc);
+        driver.quit();
+    }
+
+}
