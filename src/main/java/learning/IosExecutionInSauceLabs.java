df44897a7d2c647f753f6b3a08034ec840d11e9a
lokeshkumar.thyagarajan <LokeshKumar.Thyagarajan@hidglobal.com>
2024-05-25 00:58:27 +0530

    Day 6 code updated

--- src/main/java/learning/IosExecutionInSauceLabs.java
+++ src/main/java/learning/IosExecutionInSauceLabs.java
@@ -7,6 +7,7 @@ import org.testng.annotations.Test;
 
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.time.Duration;
 
 public class IosExecutionInSauceLabs {
     @Test
@@ -14,19 +15,21 @@ public class IosExecutionInSauceLabs {
         MutableCapabilities caps = new MutableCapabilities();
         caps.setCapability("platformName", "iOS");
         caps.setCapability("appium:app", "storage:filename=UICatalog.zip");  // The filename of the mobile app
-        caps.setCapability("appium:deviceName", "iPhone XS Simulator");
-        caps.setCapability("appium:platformVersion", "current_major");
+        caps.setCapability("appium:deviceName", "iPhone Simulator");
+        caps.setCapability("appium:platformVersion", "17.0");
         caps.setCapability("appium:automationName", "XCUITest");
         MutableCapabilities sauceOptions = new MutableCapabilities();
-        sauceOptions.setCapability("appiumVersion", "latest");
+        sauceOptions.setCapability("appiumVersion", "2.1.3");
         sauceOptions.setCapability("username", "oauth-appiummay-acf8f");
         sauceOptions.setCapability("accessKey", "693b6ee5-a268-4470-b40e-7c10e66aeaa5");
         sauceOptions.setCapability("build", "<your build id>");
         sauceOptions.setCapability("name", "<your test name>");
         sauceOptions.setCapability("deviceOrientation", "PORTRAIT");
         caps.setCapability("sauce:options", sauceOptions);
+
         URL url = new URL("https://ondemand.eu-central-1.saucelabs.com:443/wd/hub");
         IOSDriver driver = new IOSDriver(url, caps);
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
         driver.findElement(AppiumBy.accessibilityId("Action Sheets")).click();
         driver.findElement(AppiumBy.accessibilityId("Okay / Cancel")).click();
         driver.findElement(AppiumBy.accessibilityId("OK")).click();
